ea4a25e84cb373ff570668d8bdb1efc74194bd70
KurMalual <k.malual@alustudent.com>
2023-02-08 06:59:32 -0800

    102-magic_calculation.py

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# 100-print_tebahpla.py
+# 102-magic_calculation.py
 
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
