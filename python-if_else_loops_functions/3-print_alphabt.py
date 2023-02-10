5b5ffec4578172ed6b675c0be491279d93e836ad
KurMalual <k.malual@alustudent.com>
2023-02-10 05:34:03 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 3-print_alphabt.py
-
 """print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
     if chr(letter) != 'e' and chr(letter) != 'q':
