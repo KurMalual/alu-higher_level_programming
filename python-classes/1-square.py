f901125513d91a0a74dd6bcdd9fd3b1c2518a25f
KurMalual <k.malual@alustudent.com>
2023-03-17 07:10:22 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """
-THIS CODE CONTAINS NO MODULESL
+Module - 1-square
+Defines a class Square with a private instance attribute size
 """
 
 
-class Square():
+class Square:
     """
-    CLASS IS A SQUARE CLASS AND IS EMPTY
+    A class Square with private instance attribute size
     """
+    def _init_(self, size):
+        """
+        Initialize a new Square with the given size
+        Args:
+        size (int): The size of the new square object.
+        Returns:
+        None
+        """
+        self.__size = size
