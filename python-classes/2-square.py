4409a6b9368b1ff08cf6247ae2b8377e7ac41daa
KurMalual <k.malual@alustudent.com>
2023-03-17 04:29:58 -0700

    updated

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""A class that defines a square"""
+"""
+This module contains one class:
+    Square
+"""
 
 
 class Square:
-    """Create a square.
-    Private instance attributes: size
     """
-
+    This class defines a square
+    """
     def _init_(self, size=0):
-        """Initializes date."""
+        """
+        Initializes an object for the Square class.
+
+        Args:
+            size (int): size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
