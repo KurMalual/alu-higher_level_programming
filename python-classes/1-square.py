9b8786447afd86ad38df7a47defe287bb41bb283
KurMalual <k.malual@alustudent.com>
2023-03-17 04:35:56 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""A class Square that defines a square by: (based on 0-square.py)
-Private instance attribute: size
-Instantiation with size (no type/value verification)
-You are not allowed to import any module
+"""
+Module - 1-square
+Defines a class Square with a private instance attribute size
 """
 
 
 class Square:
-    """Creates a square.
-    Private instance attributes: size
     """
-
+    A class Square with private instance attribute size
+    """
     def _init_(self, size):
-        """Initializes size"""
+        """
+        Initialize a new Square with the given size
+
+        Args:
+        size (int): The size of the new square object.
+
+        Returns:
+        None
+        """
         self.__size = size
