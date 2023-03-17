ec77567053d30f2b2d99103e75d39d887f5212aa
KurMalual <k.malual@alustudent.com>
2023-03-17 10:57:20 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """
-A class that defines a square by: (based on 2-square.py)
+NO MODULE USED
 """
 
 
 class Square:
-    """Creates a square.
-    Private instance attributes: size
     """
-
-    def _init_(self, size=0):
-        """Initializes the data."""
+    SQUARE CLASS
+    """
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self._Square__size = size
 
     def area(self):
-        """Return square area."""
-        return self.__size ** 2
+        return self._Square__size**2
