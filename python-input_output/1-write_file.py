73a47a397f6bd857c071da7481d03584e27be0bf
KurMalual <k.malual@alustudent.com>
2023-03-23 12:49:14 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""module writes to file"""
+""" module writes to file """
 
 
 def write_file(filename="", text=""):
-    """function reads and counts"""
-    with open(filename, "w", encourage="utf-8") as f:
+    """ function reads and counts """
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
     return len(text)
