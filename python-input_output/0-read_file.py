6885dbf43bd0969deb75f266c47e6e540dc5380d
KurMalual <k.malual@alustudent.com>
2023-03-23 12:47:16 -0700

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""module for opening file"""
+""" module for opening file """
 
 
-def read_file(filename="""):
-    """function- opens and read passed by name"""
-    with open(filename, encording="utf-8") as f:
-       out_data = f.read()
-       print("{}".format(out_data), end="")
+def read_file(filename=""):
+    """ funciton opens and read file passed by name """
+    with open(filename, encoding="utf-8") as f:
+        out_data = f.read()
+        print("{}".format(out_data), end="")
