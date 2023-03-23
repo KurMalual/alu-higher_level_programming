905a187abcba9db8c90362c63f34914b51e19522
KurMalual <k.malual@alustudent.com>
2023-03-23 13:21:26 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-adds all arguments to a python list, and then save them to a file
+adds all arguments to a Python list, and then save them to a file
 """
 import sys
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_
-file = __import__('6-
-load_from_json_file')
-.load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 open("add_item.json", "a")
 try:
-    1 = load_from_json_file("add_item.json")
-except valveError:
-    1 = []
-save_to_json_file(1 + sys.argv[1:], "add_item.json")
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
