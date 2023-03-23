86db67ec622f18c2745a4ca73b328c4e28218316
KurMalual <k.malual@alustudent.com>
2023-03-23 11:00:06 -0700

    3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,10 +2,10 @@
 """Checks object class"""
 
 
-def inherits_from(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Checks object class
     Args:
         - obj: object to class
         - a_class: class to check
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class)
