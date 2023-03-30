ee57fb87159df244ec342b2478f817d3fb0fbe5e
KurMalual <k.malual@alustudent.com>
2023-03-30 11:20:30 -0700

    13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,8 @@
---  a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT g.`name`
+-- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
   FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
-
-       INNER JOIN `tv_shows` AS t
-       ON t.`id` = s.`show_id`
-       WHERE t.`title` = "Dexter"
- ORDER BY g.`name`;
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
