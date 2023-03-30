53b766f902d7207e466eccfcb643dde669ab74b0
KurMalual <k.malual@alustudent.com>
2023-03-30 04:57:24 -0700

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- lists all rows of a table
--- list all rows of the table first-table from the database hbtn_0c_0
+-- Lists all rows of a table
+-- List all rows of the table first_table from the database hbtn_0c_0
 SELECT * FROM first_table;
