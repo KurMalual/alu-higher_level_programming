9f0dc2d1f215517b4caf1537201e4d8c550fd952
KurMalual <k.malual@alustudent.com>
2023-03-30 05:55:25 -0700

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,2 @@
--- Lists all rows of a table
--- List all rows of the table first_table from the database hbtn_0c_0
-INSERT INTO first_table (id, name)
-VALUES (89, 'Best School');
-EOF
+-- script that inserts a new in the table first_table (database hbtn_0c_0) in your mysql server.
+INSERT INTO first_table(id, name) VALUES(89, 'Holberton School');
