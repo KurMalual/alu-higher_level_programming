d9d4582e7848f8648798fa6122ebfce65c14bb9d
KurMalual <k.malual@alustudent.com>
2023-03-30 05:46:23 -0700

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,11 @@
 -- Lists all rows of a table
 -- List all rows of the table first_table from the database hbtn_0c_0
-SELECT * FROM first_table;
+
+# Set database name as a variable
+DB_NAME="$1"
+
+# Use MySQL command to insert new row into first_table
+mysql -u root -p "$DB_NAME" <<EOF
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
+EOF
