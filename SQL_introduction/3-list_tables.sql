7b065f4589e92cde4b27f1823d015b64e535fd18
KurMalual <k.malual@alustudent.com>
2023-03-30 03:11:34 -0700

    3-list_tables.sql

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if database name is provided as argument
+if [ $# -eq 0 ]
+  then
+    echo "Please provide the database name as argument."
+    exit 1
+fi
+
+# Store database name in a variable
+DB_NAME=$1
+
+# Execute MySQL command to list tables
+mysql -e "USE ${DB_NAME}; SHOW TABLES;"
