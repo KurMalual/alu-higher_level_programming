8ed74dcf1456c95a5f17370d848ae1cd6545dca0
KurMalual <k.malual@alustudent.com>
2023-04-04 14:04:39 -0700

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Receiving content and displaying content length information
-curl -sl "1" | grep "Content-Length" | cut -d' ' -f 2
+# Receiving content and displaying content length information
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f 2
