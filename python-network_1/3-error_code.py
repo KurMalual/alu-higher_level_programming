d839ded4ff0d082e6f3ba46291faf2f74cf763de
KurMalual <k.malual@alustudent.com>
2023-04-06 09:15:57 -0700

    3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
+""" Script that takes in a URL and an email, sends a POST request
+ And displays the body of the response
 """
+
+import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    try:
-        with request.urlopen(sys.argv[1]} as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as resp:
+        print(resp.read().decode("utf-8"))
