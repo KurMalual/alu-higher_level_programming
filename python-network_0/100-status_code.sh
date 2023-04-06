ecbabf1b2e374f6f0ce6a76471271dc46ab17c03
KurMalual <k.malual@alustudent.com>
2023-04-06 11:47:49 +0000

    100-status_code.sh

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a request to a URL passed as a argument
+# Bash script that sends a request to a URL passed as a argument, and displays only the status code of the response.
 curl -sLw "%{http_code} -o /dev/null "s1"
