7c364221b370875c9428a6e3b25c3b5809b226a9
KurMalual <k.malual@alustudent.com>
2023-04-06 09:49:38 -0700

    8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" Python script that takes in a letter and sends a POST request to http:0.0.0.0:5000/seacrh_usr with the letter as a parameter """
-import requests 
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
 from sys import argv
 
 if __name__ == '__main__':
     q = argv[1] if len(argv) == 2 else ""
     url = 'http://0.0.0.0:5000/search_user'
-    r - requests.post(url, data={'q': q})
+    r = requests.post(url, data={'q': q})
     try:
         r_dict = r.json()
         id, name = r_dict.get('id'), r_dict.get('name')
