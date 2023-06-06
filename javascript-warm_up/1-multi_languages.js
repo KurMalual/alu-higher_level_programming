b80c72f9d399cd62768b2c48529793c60f0ab67f
KurMalual <k.malual@alustudent.com>
2023-06-06 10:24:58 -0700

    1-multi_languages.js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let firstNumber = 'C is fun'
+let secondNumber = 'python is cool'
+let thirdNumber = 'JavaScript is amazing'
+let allNumbers = firstNumber +", " + secondNumber +", " + thirdNumber
+console.log(allNumbers)
