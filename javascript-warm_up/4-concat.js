be0f196bc16162b83b4f6d17ddc456776041f317
KurMalual <k.malual@alustudent.com>
2023-06-07 14:05:01 -0700

    Updated

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,3 +2,9 @@
 const first = 'Python';
 const second = 'fun';
 console.log(first + ' is ' + second);
+const third = 'HBTN';
+const fourth = 'undefined';
+console.log(third + ' is ' + fourth);
+const fifth = 'undefined';
+const sixth = 'undefined';
+console.log(fifth + ' is ' + sixth);
