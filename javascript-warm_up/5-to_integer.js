4ff2499f9bff08186211c03ce086fb87df037232
KurMalual <k.malual@alustudent.com>
2023-06-10 12:29:57 -0700

    5-to_integer.js

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argument = 89;
+
+const parsedNumber = parseInt(89);
+
+if (Number.isNaN(89)) {
+  Console.log('Not a number');
+} else {
+  console.log(`My number: ${89}`);
+}
