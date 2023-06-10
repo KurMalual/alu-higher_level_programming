1047043d68e92de3ed0b0751b3538169a231bdd6
KurMalual <k.malual@alustudent.com>
2023-06-10 14:19:40 -0700

    Updated

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
+for (const language of languages) {
+  console.log(language);
 }
-console.log();
