5c0792d55b24bd5ac0d920c64d6494801ecc2689
KurMalual <k.malual@alustudent.com>
2023-06-13 15:46:59 +0000

    Updated

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
+// this script reads and prints the content of a file
 
-const fs = require('f');
+const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readfile(file, 'utf-8, function (err, data) {
-  if (err) { 
+fs.readFile(filename, 'utf-8', function (err, data) {
+  if (err) {
     console.log(err);
   } else {
     console.log(data);
-  } 
+  }
 });
