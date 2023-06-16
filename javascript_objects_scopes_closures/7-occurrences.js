f379a030b7bb37844ded9b48910223de2f60c3fb
KurMalual <k.malual@alustudent.com>
2023-06-16 06:41:07 -0700

    7-occurrences.js

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+// a function that returns the number of occurrences in a list
 
-exports.nb0ccurences = function {list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const i in list) {
     if (list[i] === searchElement) {
       count++;
     }
-    return count;
+  }
+  return count;
 };
