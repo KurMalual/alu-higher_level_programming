dd99e132828b38528f4ad8b439e4ba3e7fc7d53d
KurMalual <k.malual@alustudent.com>
2023-06-16 05:21:39 -0700

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+// This script that searches the second biggest integer in the list of arguments.
 
-if (process.argv.length > 3) {
-  const array = process.argv.sclice(2).map(Number);
-
-  array.slice(array.indexOf(Math.max.apply(null, array)), 1);
-  console.log(Math.max.apply(null, array));
-} else {
-  console.log(0):
+'use strict';
+let nextMax = 0;
+const args = process.argv.slice(2);
+if (args.length > 1) {
+  args.sort();
+  nextMax = args[args.length - 2];
 }
+console.log(nextMax);
